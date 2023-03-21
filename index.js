0d093b21d8ad25e2ff6ceb8f016d02b7d1a80bbe
Vincent Pastorino <vpastori@DESKTOP-TDG3FUD>
2023-03-21 13:44:05 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function (something = "go to the office"){
+    return `This Monday, I will ${something}.`
+}
+
+function wrapAdjective(visualFlair = "*") {
+    return function(adjective = "special"){
+        return `You are ${visualFlair}${adjective}${visualFlair}!`;
+    };
+}
